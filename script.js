3c6cb08f66d4dfc7e988c51358e87b7e7d382181
Natalia-Timote <nataliamirian.nt@gmail.com>
2024-05-05 20:35:01 -0300

    utilizando async/await e try ... catch

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 const containerVideos = document.querySelector('.videos__container');
 
-const api = fetch("http://localhost:3000/videos")
-    .then(res => res.json())
-    .then((videos) =>
-    videos.forEach((video) => {
-        containerVideos.innerHTML += `
-            <li class="videos__item">
-                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-                    <div class="description-video">
-                        <img class="img-channel" src="${video.imagem}" alt="Logo do canal">
-                        <h3 class="title-video">${video.titulo}</h3>
-                        <p class="title-channel">${video.descricao}</p>
-                    </div>
-                </li>
-            `;
-    })
-)
+async function searchShowVideos() {
+    try {
+        const search = await fetch("http://localhost:3000/videos");
+        const videos = await search.json();
+
+        videos.forEach((video) => {
+            if(video.categoria == "") {
+                throw new Error('Vídeo sem categoria');
+            }
+            containerVideos.innerHTML += `
+                <li class="videos__item">
+                        <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                        <div class="description-video">
+                            <img class="img-channel" src="${video.imagem}" alt="Logo do canal">
+                            <h3 class="title-video">${video.titulo}</h3>
+                            <p class="title-channel">${video.descricao}</p>
+                        </div>
+                    </li>
+                `;
+        })
+    } catch(error) {
+        containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}.</p>`;
+    }
+}
+
+searchShowVideos();
